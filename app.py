1dac2a07f730faae1e7614b9a6187bb2d1b43069
Denis-Bez <v417459@yandex.ru>
2022-07-21 15:22:57 +0500

    Configuration Flask

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print('Hello, hell!')
+# Flask libraries
+from flask import Flask, render_template, redirect, flash, url_for, session, request, abort
+from flask_sqlalchemy import SQLAlchemy
+
+# Configuratins and castom libraries
+from config import CONFIG
+
+# CONFIGURATION BLOCK
+SECRET_KEY = CONFIG['FLASK_SECRET_KEY']
+
+app = Flask (__name__)
+
+
+# HEANDLERS BLOCK
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
